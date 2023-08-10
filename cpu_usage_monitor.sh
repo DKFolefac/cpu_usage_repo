5131c4ff488f51dca4a6fefe9688215f58179b00
DKFolefac <fomenkykelly21@gmail.com>
2023-08-10 13:58:37 +0100

    added bash script

---
+++ cpu_usage_monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# exercise on bash script on cpu usage
+cpu_threshold=5
+email_address=fomenkykelly21@gmail.com
+email_subject="report on cpu usage"
+cpu_usage_percentage=$(top -bn1 | awk '/Cpu/ { print $2 }')  
+echo $cpu_usage_percentage
+ 
+if [[ $cpu_threshold <  $cpu_usage_percentage ]]
+then
+echo "threshold excedded"
+mail -s "$email_subject " [$email_address] <<< "CPU usage has exceeded.email sent  "
+else
+echo "threshold not exceeded"
+fi
+
